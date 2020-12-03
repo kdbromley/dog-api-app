37368b1f9c02315ac7811dc48abf059808b145c8
kd Bromley <kd.bromley1@gmail.com>
2020-12-03 13:00:15 -0700

    update script.js

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 function getDogImages(requestedNumber) {
     fetch(`https://dog.ceo/api/breeds/image/random/${requestedNumber}`)
         .then(response => response.json())
-        .then (responseJson => displayDogImages(responseJson, requestedNumber))
+        .then (responseJson => console.log(responseJson.message))
         .catch(error => alert('Uh oh! Something went wrong. Please try again.'));
 }
-
-function displayDogImages(responseJson, requestedNumber) {
-    for (let i = 0; i < requestedNumber; i++) {
-        $('.img-results').append(`<img src="${responseJson.message[i]}">`);
-    }
-}
-
 
 function handleSubmit() {
     $('form').submit(event => {
