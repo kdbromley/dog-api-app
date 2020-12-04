a10c096531aa976e4bc8524cfe30129f8c6a84af
kd Bromley <kd.bromley1@gmail.com>
2020-12-04 12:09:08 -0700

    revisions for grading

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
+
+function generateDogImagesElement(image) {
+    return `<img src="${image}" class="dog-pic">`
+}
+
+function generateDogImagesString(imageUrls) {
+    const imageGroup = imageUrls.map((image) => generateDogImagesElement(image));
+    return imageGroup.join("");
+}
+
+function renderDogImages(responseJson, requestedNumber) {
+    $('.display-results').removeClass('hidden');
+    const imageUrls = responseJson.message;
+    const dogImagesString = generateDogImagesString(imageUrls);
+    $('.img-results').html(dogImagesString);
+}
+
 function getDogImages(requestedNumber) {
     fetch(`https://dog.ceo/api/breeds/image/random/${requestedNumber}`)
         .then(response => response.json())
-        .then (responseJson => displayDogImages(responseJson, requestedNumber))
+        .then(responseJson => {
+            renderDogImages(responseJson, requestedNumber);
+            console.log(responseJson.message);
+        })
         .catch(error => alert('Uh oh! Something went wrong. Please try again.'));
-}
-
-function displayDogImages(responseJson, requestedNumber) {
-    for (let i = 0; i < requestedNumber; i++) {
-        $('.img-results').append(`<img src="${responseJson.message[i]}">`);
-    }
 }
 
 
@@ -16,6 +30,7 @@ function handleSubmit() {
     $('form').submit(event => {
         event.preventDefault();
         const requestedNumber = $('input').val();
+        $('input').val("3");
         getDogImages(requestedNumber);
     })
 }
